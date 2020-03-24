a51fa623d28256f26a45ceafdd5392e117cfb8c5
Dominic Montelongo <montelongodominic@gmail.com>
2020-03-24 14:33:51 -0700

    add jquery.flip to project folder and prepare html for scripts.js content

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function){
+$('card').flip({
+  trigger: 'manual'
+});
+
+
+
+}
+$(".flippable").click(function(){
+  $(this).toggleClass("flipme");
+});
+
+
+
+https://nnattawat.github.io/flip/
+
